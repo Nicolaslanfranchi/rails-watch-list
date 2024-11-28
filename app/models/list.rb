077add89d01dc23403a8f754b5524750c788eb82
nicolas lanfranchi <n.lanfranch@gmail.com>
2024-11-28 12:04:07 +0100

    added validations

---
+++ app/models/list.rb
@@ -0,0 +1,7 @@
+class List < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  validates :name, uniqueness: true
+  validates :name, presence: true
+  has_many :movies, through: :bookmarks
+
+end
